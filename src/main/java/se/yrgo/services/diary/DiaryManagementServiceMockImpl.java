ed3f7f47f54877a7fdd9a8552b8558af5a6a4082
GurjotKaur1501 <jotkau0115@gmail.com>
2025-04-22 14:31:08 +0200

    Update DiaryManagementServiceMockImpl.java

--- src/main/java/se/yrgo/services/diary/DiaryManagementServiceMockImpl.java
+++ src/main/java/se/yrgo/services/diary/DiaryManagementServiceMockImpl.java
@@ -1,27 +1,27 @@
 package se.yrgo.services.diary;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.ArrayList;
 
 import se.yrgo.domain.Action;
 
 public class DiaryManagementServiceMockImpl implements DiaryManagementService {
 	
-	private Set<Action>allActions= new HashSet<Action>();
+	private List<Action> actions = new ArrayList<>();
 
 	@Override
 	public void recordAction(Action action) {
-		// TODO Auto-generated method stub
-
+		actions.add(action);
 	}
 
 	//Hint: 
 	//Create a list<Action>
 	//In the for each loop going through the list use this condition: "if(action.getOwningUser().equals(requiredUser) && !action.isComplete())" to add a new action to the list. 
 	public List<Action> getAllIncompleteActions(String requiredUser) {
-		// TODO Auto-generated method stub
-		return null;
+		return new ArrayList<>(actions);
 	}
 
+	public void setActions(List<Action> actions) {
+        this.actions = actions;
+    }
 }
